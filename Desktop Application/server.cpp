a46b2a3627929c2dbc63cb63047e5003e3cbc502
IIAZYFII <18hussainasif@gmail.com>
2023-06-19 10:02:55 +0100

    Websocket only allows one connection at one time

--- Desktop Application/server.cpp
+++ Desktop Application/server.cpp
@@ -23,8 +23,8 @@ void Server::run(int &bpm, int &s_port)
                               {
                                   websocketpp::server<websocketpp::config::asio>::connection_ptr conn = endpointref->get_con_from_hdl(hdl);
                                   std::cout << "New Connection Established" << conn->get_remote_endpoint() << "\n";
-
-                                  endpointref->send(hdl, "WebSocket Connection Established", websocketpp::frame::opcode::text); });
+                                  endpointref->send(hdl, "WebSocket Connection Established", websocketpp::frame::opcode::text);
+                                  endpointref->stop_listening(); });
 
     endpoint.set_message_handler([&endpointref, &bpm, this](websocketpp::connection_hdl hdl, websocketpp::server<websocketpp::config::asio>::message_ptr msg)
                                  {
@@ -47,12 +47,15 @@ void Server::run(int &bpm, int &s_port)
     endpoint.listen(s_port);
     endpoint.start_accept();
     endpoint.run();
+  
 }
 
 void Server::stop()
 {
-    endpoint.stop_listening();
-    endpoint.stop();
+
+
+
+
 }
 
 bool Server::isNumber(std::string check_string)
